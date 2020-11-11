7d37af00e377b678493fa6c09631664cc4c00959
tubasthebest <tubasthebest@gmail.com>
2020-11-11 15:53:06 -0700

    changed shop input options to intgers

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,14 +28,11 @@ var fightOrSkip = function() {
 
 var fight = function(enemy) {
     while(enemy.health > 0 && playerInfo.health > 0) {
-        
-        while(enemy.health > 0 && playerInfo.health > 0) {
-            if (fightOrSkip()) {
-                //if true, leave fight by breaking loop
-                break;
-            }
+        if (fightOrSkip()) {
+            //if true, leave fight by breaking loop
+            break;
         }
-
+    
         // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
         var damage = randomNumber(playerInfo.attack -3, playerInfo.attack);
 
@@ -141,28 +138,22 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":   
-        case "leave":
+        case 3:
           window.alert("Leaving the store.");
-      
-          // do nothing, so function will end
           break;
         default:
           window.alert("You did not pick a valid option. Try again.");
-      
-          // call shop() again to force player to pick a valid option
           shop();
           break;
     }      
